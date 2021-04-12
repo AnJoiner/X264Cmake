8b0cc063cc3784860170f7643f090589cf25e47b
anjoiner <anjoiner@163.com>
2021-04-12 16:28:22 +0800

    新增h264转mp4

--- app/src/main/java/com/coder/x264cmake/MainActivity.java
+++ app/src/main/java/com/coder/x264cmake/MainActivity.java
@@ -5,9 +5,19 @@ import android.view.View;
 import android.widget.Button;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.FileDataSourceImpl;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.tracks.h264.H264TrackImpl;
+
 
 /**
  * @author: AnJoiner
@@ -21,31 +31,81 @@ public class MainActivity extends AppCompatActivity {
     // yuv 和 h264路径
     private String yuvPath;
     private String h264Path;
-    // 视频宽高
-    private static final  int width = 720;
-    private static final int height = 1280;
+    // 最终的mp4路径
+    private String mp4Path;
 
+    // 视频宽高
+    private int width = 720;
+    private int height = 1280;
+
+    private Button mExecuteBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button executeBtn = findViewById(R.id.execute_btn);
+        initView();
+        initParams();
+        initData();
+        initListener();
+    }
 
-        yuvPath = getExternalCacheDir() + File.separator +"test.yuv";
-        h264Path = getExternalCacheDir()+File.separator +"test.h264";
+    private void initView() {
+        mExecuteBtn = findViewById(R.id.execute_btn);
+    }
 
+    private void initParams() {
+        // 替换成你自己的视频宽高
+        width = 720;
+        height = 1280;
+
+        yuvPath = getExternalCacheDir() + File.separator + "test.yuv";
+        h264Path = getExternalCacheDir() + File.separator + "test.h264";
+        mp4Path = getExternalCacheDir() + File.separator + "test.mp4";
+
+        FileUtils.copyFilesAssets(this, "test.yuv", yuvPath);
+    }
+
+    private void initData() {
         mX264Encode = new X264Encode();
+    }
 
-        FileUtils.copyFilesAssets(this,"test.yuv",yuvPath);
 
-        executeBtn.setOnClickListener(new View.OnClickListener() {
+    private void initListener() {
+        mExecuteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mX264Encode.encode(width,height,yuvPath,h264Path,YUVFormat.YUV_420);
+                mX264Encode.encode(width, height, yuvPath, h264Path, YUVFormat.YUV_420);
+                h264ToMp4();
             }
         });
+    }
+
+    /**
+     * 将h264转成mp4
+     */
+    private void h264ToMp4(){
+        H264TrackImpl h264Track;
+        try {
+            h264Track = new H264TrackImpl(new FileDataSourceImpl(h264Path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        Movie movie = new Movie();
+        movie.addTrack(h264Track);
+
+        Container mp4file = new DefaultMp4Builder().build(movie);
+
+        FileChannel fc = null;
+        try {
+            fc = new FileOutputStream(new File(mp4Path)).getChannel();
+            mp4file.writeContainer(fc);
+            fc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
