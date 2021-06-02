5d78b5657bf5424ddf8826476068fdaccce019eb
anjoiner <anjoiner@163.com>
2021-06-02 17:58:02 +0800

    使用oes实现相机预览

--- app/src/main/java/com/coder/x264cmake/module/GLRenderer.java
+++ app/src/main/java/com/coder/x264cmake/module/GLRenderer.java
@@ -2,7 +2,6 @@ package com.coder.x264cmake.module;
 
 
 import android.graphics.SurfaceTexture;
-import android.opengl.GLES11Ext;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 
@@ -65,23 +64,7 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     private int height = 1280;
 
     public GLRenderer() {
-        vertexData = ByteBuffer.allocateDirect(vers.length * BITS_PER_FLOAT)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer();
 
-        vertexData.put(vers);
-
-
-        txtCoordData = ByteBuffer.allocateDirect(txts.length * BITS_PER_FLOAT)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer();
-
-        txtCoordData.put(txts);
-
-
-        textData = ByteBuffer.allocateDirect(texts.length * BITS_PER_FLOAT)
-                .order(ByteOrder.nativeOrder())
-                .asIntBuffer();
     }
 
 
@@ -89,93 +72,7 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         GLES20.glClearColor(0, 0, 0, 1);
 
-        program = OpenGlUtils.loadProgram(GLImageFilter.VERTEX_SHADER, GLImageFilter.FRAGMENT_SHADER);
-        if (OpenGlUtils.validateProgram(program)) {
-            // 开始使用
-            GLES20.glUseProgram(program);
 
-            // 获取定义的位置
-            aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
-            // 通知opengl去找数据
-            GLES20.glEnableVertexAttribArray(aPositionLocation);
-            // 将数据定位到开头
-            vertexData.position(0);
-            GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
-                    false, 12, vertexData);
-
-            aTxtCoordLocation = GLES20.glGetAttribLocation(program, A_TEX_COORD);
-            GLES20.glEnableVertexAttribArray(aTxtCoordLocation);
-            txtCoordData.position(0);
-            GLES20.glVertexAttribPointer(aTxtCoordLocation, 2, GLES20.GL_FLOAT,
-                    false, 8, txtCoordData);
-
-            //材质纹理初始化
-            //设置纹理层
-            GLES20.glUniform1i(GLES20.glGetUniformLocation(program, Y_TEXTURE), 0); //对于纹理第1层
-            GLES20.glUniform1i(GLES20.glGetUniformLocation(program, U_TEXTURE), 1); //对于纹理第2层
-            GLES20.glUniform1i(GLES20.glGetUniformLocation(program, V_TEXTURE), 2); //对于纹理第3层
-
-            //创建opengl纹理
-            int[] texts = new int[3];
-            GLES20.glGenTextures(3, texts,0);
-
-//            surfaceTexture = new SurfaceTexture(texts[0]);
-
-            //设置纹理属性
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texts[0]);
-            //过滤器
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
-//            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
-//            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
-            //设置纹理的格式和大小
-            GLES20. glTexImage2D(GLES20.GL_TEXTURE_2D,
-                    0,           //细节基本 0默认
-                    GLES20.GL_LUMINANCE,//gpu内部格式 亮度，灰度图
-                    width,height, //拉升到全屏
-                    0,             //边框
-                    GLES20.GL_LUMINANCE,//数据的像素格式 亮度，灰度图 要与上面一致
-                    GLES20.GL_UNSIGNED_BYTE, //像素的数据类型
-                    null                    //纹理的数据
-            );
-
-            //设置纹理属性
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texts[1]);
-            //过滤器
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
-//            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
-//            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
-            //设置纹理的格式和大小
-            GLES20. glTexImage2D(GLES20.GL_TEXTURE_2D,
-                    0,           //细节基本 0默认
-                    GLES20.GL_LUMINANCE,//gpu内部格式 亮度，灰度图
-                    width/2,height/2, //拉升到全屏
-                    0,             //边框
-                    GLES20.GL_LUMINANCE,//数据的像素格式 亮度，灰度图 要与上面一致
-                    GLES20.GL_UNSIGNED_BYTE, //像素的数据类型
-                    null                    //纹理的数据
-            );
-
-
-            //设置纹理属性
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texts[2]);
-            //过滤器
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
-//            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
-//            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
-            //设置纹理的格式和大小
-            GLES20. glTexImage2D(GLES20.GL_TEXTURE_2D,
-                    0,           //细节基本 0默认
-                    GLES20.GL_LUMINANCE,//gpu内部格式 亮度，灰度图
-                    width/2,height/2, //拉升到全屏
-                    0,             //边框
-                    GLES20.GL_LUMINANCE,//数据的像素格式 亮度，灰度图 要与上面一致
-                    GLES20.GL_UNSIGNED_BYTE, //像素的数据类型
-                    null                    //纹理的数据
-            );
-        }
     }
 
     @Override
@@ -185,18 +82,6 @@ public class GLRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-//        GLES20.glUniform4f(uColorLocation,1.0f,1.0f,1.0f,1.0f);
-//        GLES20.glDrawArrays(GLES20.GL_TRIANGLES,0,6);
-//
-//        GLES20.glUniform4f(uColorLocation,1.0f,0.0f,0.0f,1.0f);
-//        GLES20.glDrawArrays(GLES20.GL_LINES,6,2);
-//
-//        GLES20.glUniform4f(uColorLocation,0.0f,0.0f,1.0f,1.0f);
-//        GLES20.glDrawArrays(GLES20.GL_POINTS,8,1);
-//
-//        GLES20.glUniform4f(uColorLocation,1.0f,0.0f,0.0f,1.0f);
-//        GLES20.glDrawArrays(GLES20.GL_POINTS,9,1);
     }
 
 }
--- app/src/main/java/com/coder/x264cmake/module/filter/GLImageFilter.java
+++ app/src/main/java/com/coder/x264cmake/module/filter/GLImageFilter.java
@@ -1,165 +1,80 @@
 package com.coder.x264cmake.module.filter;
 
 
+import android.opengl.GLES11Ext;
 import android.opengl.GLES20;
 
-import com.coder.x264cmake.utils.OpenGlUtils;
-
-import java.nio.FloatBuffer;
-import java.util.LinkedList;
+import javax.microedition.khronos.opengles.GL10;
 
 public class GLImageFilter {
 
+    // 定点着色器
     public static final String VERTEX_SHADER = "" +
-            "attribute vec4 aPosition; //顶点坐标\n" +
-            "attribute vec2 aTexCoord; //材质顶点坐标\n" +
-            "varying vec2 vTexCoord;   //输出的材质坐标\n" +
-            "void main(){\n" +
-            "    vTexCoord = vec2(aTexCoord.x,1.0-aTexCoord.y);\n" +
-            "    gl_Position = aPosition;\n" +
-            "}";
-    public static final String FRAGMENT_SHADER = "" +
-            "precision mediump float;    //精度\n" +
-            "varying vec2 vTexCoord;     //顶点着色器传递的坐标\n" +
-            "uniform sampler2D yTexture; //输入的材质（不透明灰度，单像素）\n" +
-            "uniform sampler2D uTexture;\n" +
-            "uniform sampler2D vTexture;\n" +
-            "void main(){\n" +
-            "    vec3 yuv;\n" +
-            "    vec3 rgb;\n" +
-            "    yuv.r = texture2D(yTexture,vTexCoord).r;\n" +
-            "    yuv.g = texture2D(uTexture,vTexCoord).r - 0.5;\n" +
-            "    yuv.b = texture2D(vTexture,vTexCoord).r - 0.5;\n" +
-            "    rgb = mat3(1.0,     1.0,    1.0,\n" +
-            "               0.0,-0.39465,2.03211,\n" +
-            "               1.13983,-0.58060,0.0)*yuv;\n" +
-            "    //输出像素颜色\n" +
-            "    gl_FragColor = vec4(rgb,1.0);\n" +
-            "}";
-
-
-    public static final String NO_FILTER_VERTEX_SHADER = "" +
-            "attribute vec4 position;\n" +
-            "attribute vec4 inputTextureCoordinate;\n" +
-            " \n" +
+            "attribute vec2 position;\n" +
+            "attribute vec2 inputTextureCoordinate;\n" +
             "varying vec2 textureCoordinate;\n" +
-            " \n" +
             "void main()\n" +
             "{\n" +
+            "    textureCoordinate = inputTextureCoordinate;\n" +
             "    gl_Position = position;\n" +
-            "    textureCoordinate = inputTextureCoordinate.xy;\n" +
             "}";
-    public static final String NO_FILTER_FRAGMENT_SHADER = "" +
-            "varying highp vec2 textureCoordinate;\n" +
-            " \n" +
-            "uniform sampler2D inputImageTexture;\n" +
-            " \n" +
-            "void main()\n" +
-            "{\n" +
-            "     gl_FragColor = texture2D(inputImageTexture, textureCoordinate);\n" +
-            "}";
-    private final LinkedList<Runnable> mRunnables;
-    private final String vertexShader;
-    private final String fragmentShader;
-    private int glProgramId;
-    private int glAttribPosition;
-    private int glUniformTexture;
-    private int glAttribTextureCoordinate;
-    private int outputWidth;
-    private int outputHeight;
-    private boolean isInitialized;
 
-    public GLImageFilter() {
-        this(NO_FILTER_VERTEX_SHADER,NO_FILTER_FRAGMENT_SHADER);
-    }
+    // oes片元着色器
+    public static final String TexOES_FRAGMENT_SHADER = ""+
+            "#extension GL_OES_EGL_image_external : require\n" +
+            "precision mediump float;\n" +
+            "varying vec2 textureCoordinate;\n" +
+            "uniform samplerExternalOES vTexture;\n" +
+            "void main() {\n" +
+            "    gl_FragColor = texture2D( vTexture, textureCoordinate );\n" +
+            "}\n";
 
-    public GLImageFilter(String vertexShader, String fragmentShader) {
-        mRunnables = new LinkedList<>();
-        this.vertexShader = vertexShader;
-        this.fragmentShader = fragmentShader;
-    }
-
-    private void init() {
-        onInit();
-        onInitialized();
-    }
+    // 2d片元着色器
+    public static final String Tex2D_FRAGMENT_SHADER = ""
+            + "precision mediump float;\n"
+            + "uniform sampler2D vTexture;\n"
+            + "varying vec2 textureCoordinate;\n"
+            + "void main() {\n"
+            + "  gl_FragColor = texture2D( vTexture, textureCoordinate );\n" + "}";
 
 
-    public void onInit() {
-        glProgramId = OpenGlUtils.loadProgram(vertexShader, fragmentShader);
-        glAttribPosition = GLES20.glGetAttribLocation(glProgramId, "position");
-        glUniformTexture = GLES20.glGetUniformLocation(glProgramId, "inputImageTexture");
-        glAttribTextureCoordinate = GLES20.glGetAttribLocation(glProgramId, "inputTextureCoordinate");
-        isInitialized = true;
-    }
+    // 材质顶点坐标
+    private final float vertices[] = {
+            -1, -1,
+            -1,  1,
+            1, -1,
+            1,  1 };
 
-    public void onInitialized() {
-    }
+    // oes纹理坐标
+    private final float texCoordOES[] = {
+            0,  1,
+            0,  0,
+            1,  1,
+            1,  0 };
 
-    public void ifNeedInit() {
-        if (!isInitialized) init();
-    }
-
-    public final void destroy() {
-        isInitialized = false;
-        GLES20.glDeleteProgram(glProgramId);
-        onDestroy();
-    }
-
-    public void onDestroy() {
-
-    }
+    // tex 2d纹理坐标
+    private final float texCoord2D[] = {
+            0,  0,
+            0,  1,
+            1,  0,
+            1,  1 };
 
     /**
-     * opengl绘制数据
-     * @param textureId 纹理id
-     * @param cubeBuffer 顶点
-     * @param textureBuffer 片元
+     * 创建oes纹理id
+     *
+     * @return 纹理id
      */
-    public void glDraw(int textureId, FloatBuffer cubeBuffer, FloatBuffer textureBuffer){
-        GLES20.glUseProgram(glProgramId);
-        if (!isInitialized) {
-            return;
-        }
-        cubeBuffer.position(0);
-        GLES20.glVertexAttribPointer(glAttribPosition, 2, GLES20.GL_FLOAT, false, 0, cubeBuffer);
-        GLES20.glEnableVertexAttribArray(glAttribPosition);
-
-        textureBuffer.position(0);
-        GLES20.glVertexAttribPointer(glAttribTextureCoordinate, 2, GLES20.GL_FLOAT, false, 0,
-                textureBuffer);
-        GLES20.glEnableVertexAttribArray(glAttribTextureCoordinate);
-
-        if (textureId != OpenGlUtils.NO_TEXTURE) {
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
-            GLES20.glUniform1i(glUniformTexture, 0);
-        }
-
-        onDrawArraysPre();
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
-        GLES20.glDisableVertexAttribArray(glAttribPosition);
-        GLES20.glDisableVertexAttribArray(glAttribTextureCoordinate);
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+    private int createTextureOesID() {
+        int[] texture = new int[1];
+        GLES20.glGenTextures(1, texture, 0);
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, texture[0]);
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
+        return texture[0];
     }
 
 
-    protected void onDrawArraysPre() {
 
-    }
-
-    protected void runPendingOnDrawTasks() {
-        synchronized (mRunnables) {
-            while (!mRunnables.isEmpty()) {
-                mRunnables.removeFirst().run();
-            }
-        }
-    }
-
-
-    protected void runOnDraw(final Runnable runnable) {
-        synchronized (mRunnables) {
-            mRunnables.addLast(runnable);
-        }
-    }
 }
